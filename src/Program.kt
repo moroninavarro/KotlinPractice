911f75cdc084144f03a91a998deef1b4e370f7d0
moroninavarro <moronialanis23@gmail.com>
2026-03-11 22:15:49 -0700

    debugging and fixing my code

--- src/Program.kt
+++ src/Program.kt
@@ -1,85 +1,104 @@
 fun main() {
     println("Welcome to my Basis Calculator!")
-    // Welcome!
-    // Basically, the functionality of this calculator consist of inserting the first number
-    // then you chose an operation
-    // and after that a second number, at the end you will receive the result of your numbers according
-    //to the operation you chose
+// Welcome!
+// Basically, the functionality of this calculator consist of inserting the first number
+// then you chose an operation
+// and after that a second number, at the end you will receive the result of your numbers according
+//to the operation you chose
     println("Instructions:")
     println("Select the first number, then you will chose an operation, and then the second number.")
     println("")
 
-
-    println("Enter the First number: ")
-    val number1 = readLine()!!.toInt()
-
-    clearScreen()
-    menu()
-    val operation = readLine()!!.toInt()
+//This variable store our class calculator with all the logic.
+    val app = calculator()
+//Now we print the variable displaying all our logic program.
+    println(app)
+}
 
 
-    clearScreen()
-    println("Enter the Second number: ")
-    val number2 = readLine()!!.toInt()
+//My calculator function basically contains all the callings of my classes, this
+//function contains all the logic of the calculator functionality. Asking the user for numbers and
+//the operation he wants to do. At the end there is an option to repeat the process and to do another operation
+fun calculator(){
+    do{
+        print("Enter the First number: ")
+        val number1 = readLine()!!.toInt()
+
+        clearScreen()
+        menu()
+        val operation = readLine()!!.toInt()
 
 
-    when (operation) {
-        1 -> Sum(number1, number2).showSum()
-        2 -> Subtraction(number1, number2).showSubtraction()
-        3 -> Multiply(number1, number2).showMultiply()
-        4 -> Divide(number1, number2).showDivide()
-        else -> println("Invalid operation")
-    }
+        clearScreen()
+        print("Enter the Second number: ")
+        val number2 = readLine()!!.toInt()
+
+
+        when (operation) {
+            1 -> Sum(number1, number2).showSum()
+            2 -> Subtraction(number1, number2).showSubtraction()
+            3 -> Multiply(number1, number2).showMultiply()
+            4 -> Divide(number1, number2).showDivide()
+            else -> println("Invalid operation")
+        }
+
+        print("Do another operation? (y/n) ")
+        val again = readln()
+    } while (again == "y" || again == "yes" || again == "Yes")
 
 }
 
 
 
-
-
-//My classes
+//My clearScreen function help us to clean the screen printing 50 spaces after the
+//user select the numbers and select an operation
 fun clearScreen() {
-    repeat(30){
-        println()
+  repeat(50)
+  {println()}
     }
-}
 
+//This function is my menu, this allows the user to see all the available operations and select one.
 fun menu(){
     println("Calculator")
     println("1. Sum")
     println("2. Subtraction")
     println("3. Multiply")
     println("4. Divide")
-    println("Choose an operation:")
-
-
+    print("Choose an operation: ")
 }
 
+//My Sum class has 2 arguments for the numbers that the user chooses, then the function contains
+//all the logic to sum both numbers and print the result
 class Sum(val number1:Int, val number2:Int){
     fun showSum() {
         val result = number1 + number2
-        println("Result = $result")
+        println("Result $number1 + $number2 = $result")
     }
 }
 
+//My Subtraction class has 2 arguments for the numbers that the user chooses, then the function
+//contains all the logic to subtract the number2 from number1 and print the result
 class Subtraction(val number1:Int, val number2:Int){
     fun showSubtraction() {
         val result = number1 - number2
-        println("Result = $result")
+        println("Result $number1 - $number2 = $result")
     }
 }
 
-
+//My Multiply class has 2 arguments for the numbers that the user chooses, then the function
+//contains all the logic to multiply both numbers and print the result
 class Multiply(val number1:Int, val number2:Int){
     fun showMultiply() {
         val result = number1 * number2
-        println("Result = $result")
+        println("Result $number1 * $number2 = $result")
     }
 }
 
+//My Divide class has 2 arguments for the numbers that the user chooses, then the function
+//contains all the logic to divide number1 by number2 and print the result
 class Divide(val number1:Int, val number2:Int){
     fun showDivide() {
         val result = number1 / number2
-        println("Result = $result")
+        println("Result $number1 / $number2 = $result")
     }
 }
